cdfc924ba56544899f42c44495d5f92e6f2a7424
khansa <khansaabashira13@student.uns.ac.id>
2023-03-06 10:21:35 +0700

    acha

---
+++ khansa.py
@@ -0,0 +1,12 @@
+nama_depan = "khansa"
+nama_blk = "bashira"
+
+angka = 10
+desimal = 0.5
+boolean = True
+kelas_D = ["khansa", "ksl d", "dll"]
+data = {"nama": "khansa", "kelas": "KLS D"}
+
+print(nama_depan)
+print("nama saya adalah khansa")
+print(f"nama saya adalah {nama_depan} {nama_blk} dari kelas {data['kelas']}") 
