15c51d88a1b7aaf3db03e156b4d6be0654ebfcca
khansa <khansaabashira13@student.uns.ac.id>
2023-03-12 17:24:59 +0700

    Create Exercise-01.py

---
+++ Exercise-01.py
@@ -0,0 +1,11 @@
+print("haloo nama saya Khansa Bashira Pondavi")
+print("saya merupakan mahasiswa Teknik Industri UNS dengan NIM I0322136")
+print('''Saat ini saya tinggal di Kota Surakarta area sekitar Universitas Sebelas Maret, lebih lengkapnya di 
+Kost Omah Djawi Kecamatan Jebres. Sebelum saya memutuskan untuk tinggal di Kota Surakarta, saya menghabiskan masa kecil 
+dan remaja saya di daerah yang katanya orang-orang adalah planet lain yaitu Kota Bekasi. Kota ini merupakan bagian dari 
+provinsi Jawa Barat dan menjadi kota dengan jumlah penduduk terbanyak keempat di Indonesia. Dikarenakan banyaknya penduduk, 
+kota ini tentu saja tidak luput dari kemacetan dan polusi. Saya lahir di Banda aceh yang memiliki banyak pantai seperti 
+Pantai Lampuuk, Pantai Ulee Lheue, dan banyak lagi. daerah ini juga merupakan dearah penghasil barang tambang, antara lain 
+minyak bumi, gas alam, dan emas. Harapan saya sebagai lulusan Teknik Industri adalah dapat lulus tepat waktu dengan IPK 
+minimal 3.5, dapat diterima kerja di perusahaan yang cocok, dan saya ingin dapat menciptakan lapangan pekerjaan 
+seluas-luasnya.''')
