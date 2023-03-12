f22fe00863113936f7dcf6909c4a9461f6a44139
khansabshr <127003761+khansabshr@users.noreply.github.com>
2023-03-12 21:03:52 +0700

    I0322136_Excercise-01.py
    file tugas 1 yang benar

---
+++ Excercise-01.py
@@ -0,0 +1,18 @@
+nama           = "Khansa Bashira Pondavi"
+NIM            = "I0322136"
+print("==========================================")
+print("Perkenalkan saya", nama)
+print("Saya merupakan mahasiswa Teknik Industri dengan NIM", NIM)
+
+Tempat_Tinggal = "Bekasi"
+print("saya tinggal di", Tempat_Tinggal)
+print('''
+Bekasi merupakan kota yang termasuk bagian dari Provinsi Jawa Barat dan menjadi kota dengan jumlah penduduk terbanyak 
+keempat di Indonesia. Maka dari itu kota ini termasuk kota metropolis, yakni banyak terdapat tenaga kerja, perumahan,
+masyarakat bermatapencaharian industri, dan lainnya. seperti kota-kota lainnya, bekasi tidak luput dari kemacetan 
+dan polusi udara.
+''')
+
+harapan = "mendapat banyak skill, pengalaman, dan ilmu dan juga dapat bekerja di perusahaan yang cocok dan menjanjikan"
+print("Harapan saya setelah menjadi lulusan Teknik Industri adalah", harapan)
+print("==========================================")
